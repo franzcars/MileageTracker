7d2ba6cfb6b28c356743129ca1747b15bbf61c06
Justin Franz <franzcars@gmail.com>
2018-08-29 09:32:56 -0500

    Updated

--- MileageTracker/app.js
+++ MileageTracker/app.js
@@ -6,6 +6,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var uuid = require("uuid");
+var AWS = require("aws-sdk");
+var fs = require('fs');
 
 //var routes = require('./public/views/index.html');
 //var users = require('./routes/users');
@@ -27,8 +30,18 @@ app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 app.all('/', (req, res) => {
-     console.log(req.body.Vin);
-
+    //console.log(typeof req.body.Vin + ' Here');
+    console.log(req.body);
+    if (req.body.Vin) {
+        if (req.body.Check) {
+            console.log("")
+            handleSearch(req);
+        }
+        //console.log(req.body.Vin);
+        else {
+            handleUpdate(req);
+        }
+    }
     res.render('index', {
         title: 'Justin'
     }); });
@@ -74,3 +87,73 @@ app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
+
+function handleUpdate(req) {
+    AWS.config.update({
+        region: "us-east-2"
+    });
+    var docClient = new AWS.DynamoDB.DocumentClient();
+
+    var table = "Mileage";
+    var vin = req.body.Vin;
+    var mileage = parseInt(req.body.Mileage);
+    var car = "Evo";
+    var color = "White";
+    console.log(vin);
+    console.log(mileage);
+    var params = {
+        TableName: table,
+        Item: {
+            "id": uuid.v1(),
+            "Vin": vin,
+            "Mileage": mileage,
+            "Color": color,
+            "Car": car
+        }
+    };
+
+    docClient.put(params, function (err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            var stuff = JSON.stringify(data);
+            console.log(data);
+            console.log(stuff.Item);
+
+
+        }
+    });
+}
+
+function handleSearch(req) {
+    AWS.config.update({
+        region: "us-east-2"
+    });
+    var docClient = new AWS.DynamoDB.DocumentClient();
+
+    var table = "Mileage";
+    var vin = req.body.Vin;
+    var car = "GTR";
+    var color = "Silver";
+    // console.log(vin);
+    var params = {
+        TableName: table,
+        FilterExpression: 'Vin = :thisvin',
+        ExpressionAttributeValues: {
+            ":thisvin": vin
+        }
+    };
+
+    docClient.scan(params, function (err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            var stuff = JSON.stringify(data);
+            console.log(data.Items.length);
+            for (var x = 0; x < data.Items.length; x++) {
+                console.log(JSON.stringify(data.Items[x]));
+            }
+        }
+    });
+
+}
